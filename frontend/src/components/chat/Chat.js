4ed367ead2bfbed87ef97a5f4cb350e126711938
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2023-11-28 17:08:36 +0100

    Update ChatBox component to include a send button click event

--- frontend/src/components/chat/Chat.js
+++ frontend/src/components/chat/Chat.js
@@ -9,128 +9,191 @@ import Step3 from "../../images/chat/step3.svg";
 import Step4 from "../../images/chat/step4.svg";
 import Step5 from "../../images/chat/step5.svg";
 import Billie from "../../images/chat/billie.svg";
-import Information from "../../images/chat/information.svg"; 
-
+import Information from "../../images/chat/information.svg";
+import { useState } from "react";
 
 const Chat = () => {
-    return (
-        <div className="flex bg-[#F6F6F6] w-screen h-screen">
-            <ChatNav />
+	const sendBtnClick = () => {
+		setMessages((prevMessages) => [
+			...prevMessages,
+			{ user: "user", msg: "age is " },
+		]);
 
-            <div className='flex flex-col w-3/4 border border-black'>
+		if (personal_info === "") {
+			setMessages((prevMessages) => [
+				...prevMessages,
+				{ user: "ai", msg: "So what are the symptoms you're feeling?" },
+			]);
+		}
+		console.log("clicked");
+	};
+	const [messages, setMessages] = useState([]);
+	const [personal_info, setPinfo] = useState("");
+	return (
+		<div className="flex bg-[#F6F6F6] w-screen h-screen">
+			<ChatNav />
 
-                <div className=''>
-                    <ChatHeader />
-                </div>
+			<div className="flex flex-col w-3/4 border border-black">
+				<div className="">
+					<ChatHeader />
+				</div>
 
-                <div className='overflow-y-scroll'>
-                    <div className='text-center pt-10 mb-20'>
-                        <p className='text-4xl pb-6'>IT’s Billie your favourite health assistant</p>
-                        <div className='flex justify-center gap-2 text-sm'>
-                            <div className='flex flex-col items-start'>
-                                <div className='flex items-center'>
-                                    <img src={Check} alt="A check circle" className='w-10 h-10' />
-                                    <img src={Greenbar} alt="A green bar"  className='w-28 h-3' />
-                                </div>
-                                <div>
-                                    <p>Introduction</p>
-                                </div>
-                            </div>
-                            <div className='flex flex-col items-start'>
-                                <div className='flex items-center'>
-                                    <img src={Step2} alt="Second step" className='w-10 h-10' />
-                                    <img src={Whitebar} alt="A white bar"  className='w-28 h-3' />
-                                </div>
-                                <div>
-                                    <p>User assesment</p>
-                                </div>
-                            </div>
-                            <div className='flex flex-col items-start'>
-                                <div className='flex items-center'>
-                                    <img src={Step3} alt="Third step" className='w-10 h-10' />
-                                    <img src={Whitebar} alt="A white bar"  className='w-28 h-3'/>
-                                </div>
-                                <div>
-                                    <p>Symptom check</p>
-                                </div>
-                            </div>
-                            <div className='flex flex-col items-start'>
-                                <div className='flex items-center'>
-                                    <img src={Step4} alt="Fourth step" className='w-10 h-10' />
-                                    <img src={Whitebar} alt="A white bar" className='w-28 h-3' />
-                                </div>
-                                <div>
-                                    <p>Diagnosis</p>
-                                </div>
-                            </div>
-                            <div className='flex flex-col items-start'>
-                                <div className='flex items-center'>
-                                    <img src={Step5} alt="Fifth step" className='w-10 h-10' />
-                                    <img src={Whitebar} alt="A white bar" className='w-28 h-3' />
-                                </div>
-                                <div>
-                                    <p>Recommendation</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+				<div className="overflow-y-scroll">
+					<div className="text-center pt-10 mb-20">
+						<p className="text-4xl pb-6">
+							IT’s Billie your favourite health assistant
+						</p>
+						<div className="flex justify-center gap-2 text-sm">
+							<div className="flex flex-col items-start">
+								<div className="flex items-center">
+									<img
+										src={Check}
+										alt="A check circle"
+										className="w-10 h-10"
+									/>
+									<img
+										src={Greenbar}
+										alt="A green bar"
+										className="w-28 h-3"
+									/>
+								</div>
+								<div>
+									<p>Introduction</p>
+								</div>
+							</div>
+							<div className="flex flex-col items-start">
+								<div className="flex items-center">
+									<img
+										src={Step2}
+										alt="Second step"
+										className="w-10 h-10"
+									/>
+									<img
+										src={Whitebar}
+										alt="A white bar"
+										className="w-28 h-3"
+									/>
+								</div>
+								<div>
+									<p>User assesment</p>
+								</div>
+							</div>
+							<div className="flex flex-col items-start">
+								<div className="flex items-center">
+									<img
+										src={Step3}
+										alt="Third step"
+										className="w-10 h-10"
+									/>
+									<img
+										src={Whitebar}
+										alt="A white bar"
+										className="w-28 h-3"
+									/>
+								</div>
+								<div>
+									<p>Symptom check</p>
+								</div>
+							</div>
+							<div className="flex flex-col items-start">
+								<div className="flex items-center">
+									<img
+										src={Step4}
+										alt="Fourth step"
+										className="w-10 h-10"
+									/>
+									<img
+										src={Whitebar}
+										alt="A white bar"
+										className="w-28 h-3"
+									/>
+								</div>
+								<div>
+									<p>Diagnosis</p>
+								</div>
+							</div>
+							<div className="flex flex-col items-start">
+								<div className="flex items-center">
+									<img
+										src={Step5}
+										alt="Fifth step"
+										className="w-10 h-10"
+									/>
+									<img
+										src={Whitebar}
+										alt="A white bar"
+										className="w-28 h-3"
+									/>
+								</div>
+								<div>
+									<p>Recommendation</p>
+								</div>
+							</div>
+						</div>
+					</div>
 
-                    <div id="messages">
-                        <div className='w-[80%] items-start flex mx-auto gap-10 mb-10'>
-                            
-                            <img src={Billie} alt="Billie's logo" />
-                            
-                            <div className='flex flex-col'>
-                                <p className='bg-[#C5D4D3] mb-8 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl'>Hey i am Billie your favourite health assistant. I’m best at helping you with recieving your medical symptoms and Giving you a quick response on your possible health condition 🤩</p>
-                                <p className='bg-[#C5D4D3] mb-2 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl'>Whats is your age and sex?</p>
-                                <div className='flex gap-1 text-[#1e92e6]'>
-                                    <img src={Information} alt='information' />
-                                    <p className='text-base'>Why do i need this information?</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+					<div id="messages">
+						<div className="w-[80%] items-start flex mx-auto gap-10 mb-10">
+							<img src={Billie} alt="Billie's logo" />
 
-                    <div id="messages">
-                        <div className='w-[80%] items-start flex mx-auto gap-10 mb-10'>
-                            
-                            <img src={Billie} alt="Billie's logo" />
-                            
-                            <div className='flex flex-col'>
-                                <p className='bg-[#C5D4D3] mb-8 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl'>Hey i am Billie your favourite health assistant. I’m best at helping you with recieving your medical symptoms and Giving you a quick response on your possible health condition 🤩</p>
-                                <p className='bg-[#C5D4D3] mb-2 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl'>Whats is your age and sex?</p>
-                                <div className='flex gap-1 text-[#1e92e6]'>
-                                    <img src={Information} alt='information' />
-                                    <p className='text-base'>Why do i need this information?</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+							<div className="flex flex-col">
+								<p className="bg-[#C5D4D3] mb-8 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl">
+									Hey i am Billie your favourite health
+									assistant. I’m best at helping you with
+									recieving your medical symptoms and Giving
+									you a quick response on your possible health
+									condition 🤩
+								</p>
+								<p className="bg-[#C5D4D3] mb-2 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl">
+									Whats is your age and sex?
+								</p>
+								<div className="flex gap-1 text-[#1e92e6]">
+									<img src={Information} alt="information" />
+									<p className="text-base">
+										Why do i need this information?
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					{messages.map((message, key) => {
+						console.log(message.msg);
+						if (message.user == "ai") {
+							return (
+								<div key={key} id="messages">
+									<div className="w-[80%] items-start flex mx-auto gap-10 mb-10">
+										<img src={Billie} alt="Billie's logo" />
+										<div className="flex flex-col">
+											<p className="bg-[#C5D4D3] mb-2 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl">
+												{message.msg}
+											</p>
+										</div>
+									</div>
+								</div>
+							);
+						} else {
+							return (
+								<div key={key} id="messages">
+									<div className="w-[80%] items-start flex mx-auto gap-10 mb-10">
+										<img src={Billie} alt="Billie's logo" />
+										<div className="flex flex-col">
+											<p className="bg-[#C5D4D3] mb-2 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl">
+												{message.msg}
+											</p>
+										</div>
+									</div>
+								</div>
+							);
+						}
+					})}
+				</div>
 
-                    <div id="messages">
-                        <div className='w-[80%] items-start flex mx-auto gap-10 mb-10'>
-                            
-                            <img src={Billie} alt="Billie's logo" />
-                            
-                            <div className='flex flex-col'>
-                                <p className='bg-[#C5D4D3] mb-8 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl'>Hey i am Billie your favourite health assistant. I’m best at helping you with recieving your medical symptoms and Giving you a quick response on your possible health condition 🤩</p>
-                                <p className='bg-[#C5D4D3] mb-2 py-4 px-6 rounded-3xl rounded-tl-none w-[100%] text-xl'>Whats is your age and sex?</p>
-                                <div className='flex gap-1 text-[#1e92e6]'>
-                                    <img src={Information} alt='information' />
-                                    <p className='text-base'>Why do i need this information?</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className=''>
-                    <ChatBox />
-                </div>
-            </div>
-        </div>
-    )
-}
-
+				<div className="">
+					<ChatBox sendBtnClick={sendBtnClick} />
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Chat;
--- frontend/src/components/chat/subcomponent/ChatBox.js
+++ frontend/src/components/chat/subcomponent/ChatBox.js
@@ -2,23 +2,27 @@ import Microphone from "../../../images/chat/chatbox/microphone.svg";
 import Attach from "../../../images/chat/chatbox/attach.svg";
 import Send from "../../../images/chat/chatbox/send.svg";
 
-const ChatBox = () => {
-    return (
-        <div className="bg-[#FFFFFF] z-50 w-[100%] py-6 flex items-center justify-center">
-            <div className="flex items-center justify-center border w-[85%] py-4 px-6 border-[#7F7D7D] rounded-full">
-                <textarea
-                    rows={1}
-                    placeholder="Send a message"
-                    className="outline-none overflow-hidden placeholder:text-xl text-xl w-[90%] min-h-min max-h-[84px]" 
-                />
-                <div className="flex gap-2 w-[15%]">
-                    <img src={Microphone} alt="A microphone icon" />
-                    <img src={Attach} alt="An attach icon" />
-                    <img src={Send} alt="A send icon" />
-                </div>
-            </div>
-        </div>
-    )
-}
+const ChatBox = ({ sendBtnClick }) => {
+	return (
+		<div className="bg-[#FFFFFF] z-50 w-[100%] py-6 flex items-center justify-center">
+			<div className="flex items-center justify-center border w-[85%] py-4 px-6 border-[#7F7D7D] rounded-full">
+				<textarea
+					rows={1}
+					placeholder="Send a message"
+					className="outline-none overflow-hidden placeholder:text-xl text-xl w-[90%] min-h-min max-h-[84px]"
+				/>
+				<div className="flex gap-2 w-[15%]">
+					<img src={Microphone} alt="A microphone icon" />
+					<img src={Attach} alt="An attach icon" />
+					<img
+						onClick={() => sendBtnClick()}
+						src={Send}
+						alt="A send icon"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ChatBox;
